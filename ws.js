2f4b324c1be8bca384222a867ab3ce56a711e56b
Samuel Fabrizio <samuelfabrizio@gmail.com>
2019-08-29 01:16:28 +0200

    [ADD] server: request data every 8 hours

--- ws.js
+++ ws.js
@@ -3,21 +3,29 @@ const spawn = require("child_process").spawn;
 const WebSocketServer = require('ws').Server,
   wss = new WebSocketServer({
     port: 40510
-  }) 
+  })
+
+const requestData = (ws) => {
+  const process = spawn('python', ["./check.py", ]);
+  process.stdout.on('data', data => {
+    const res = data.toString();
+    console.log(res);
+    ws.send(res);
+  })
+}
 
 wss.on('connection', function (ws) {
   ws.on('message', (msg) => {
     console.log('received: %s', msg)
     if (msg === 'connect') {
       ws.send('connected')
+      setInterval(() => {
+        console.log('checking now..')
+        requestData(ws);
+      }, 2.88e+7) //every 8 hours
     } else if (msg === 'check') {
       ws.send('checking');
-      const process = spawn('python',["./check.py",] ); 
-      process.stdout.on('data', data => { 
-        const res = data.toString();
-        console.log(res);
-        ws.send(res); 
-      })
+      requestData(ws)
     }
   })
 })
